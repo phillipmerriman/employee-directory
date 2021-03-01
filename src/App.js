9dac302bbeb0f7d2046439ff30755b1b3388716a
Phillip Merriman <phillipmerriman@gmail.com>
2021-03-01 10:18:36 -0600

    updated App.js to make it a class. Added sort button

--- src/App.js
+++ src/App.js
@@ -3,24 +3,24 @@ import React, { Component } from "react";
 import employees from "./employees.json";
 import EmployeeTable from "./component/EmployeeTable";
 import TableHeader from "./component/TableHeader";
+import SortButton from "./component/SortButton";
 
 class App extends Component {
   // Setting this.state.friends to the friends json array
   state = {
     employees,
   };
-
   removeFriend = (id) => {
     // Filter this.state.employees for employees with an id not equal to the id being removed
     const employees = this.state.employees.filter((friend) => friend.id !== id);
     // Set this.state.employees equal to the new employees array
     this.setState({ employees });
   };
-
   // Map over this.state.employees and render a FriendCard component for each friend object
   render() {
     return (
       <div>
+        <SortButton />
         <table className="table table-striped table-dark">
           <TableHeader employee={employees[0]} />
           <tbody>
@@ -43,5 +43,4 @@ class App extends Component {
     );
   }
 }
-
 export default App;
---
+++ src/component/SortButton.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function SortButton(props) {
+    return (
+        <div>
+            <button type="button" class="btn btn-primary btn-lg btn-block">Sort Employees by Name!</button>
+        </div>
+    );
+}
+
+export default SortButton;
