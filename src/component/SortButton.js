0ddf1b1916be8e9065c5bcaef8315ef821a49c08
Phillip Merriman <phillipmerriman@gmail.com>
2021-03-01 10:31:04 -0600

    updated sort button

--- src/component/SortButton.js
+++ src/component/SortButton.js
@@ -1,11 +1,27 @@
 import React from "react";
 
 function SortButton(props) {
-    return (
-        <div>
-            <button type="button" class="btn btn-primary btn-lg btn-block">Sort Employees by Name!</button>
-        </div>
-    );
+  return (
+    <div>
+      <h3>Sort By:</h3>
+      <div class="btn-group btn-group-toggle" data-toggle="buttons">
+        <label class="btn btn-secondary active">
+          <input
+            type="radio"
+            name="options"
+            id="option1"
+            autocomplete="off"
+            checked
+          />{" "}
+          Id
+        </label>
+        <label class="btn btn-secondary">
+          <input type="radio" name="options" id="option2" autocomplete="off" />{" "}
+          Name
+        </label>
+      </div>
+    </div>
+  );
 }
 
 export default SortButton;
